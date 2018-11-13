47e3e7582c662e1b252b76ff168bb533bd3146d2
anton.gorbunov01 <crayfish921@gmail.com>
2018-11-13 16:34:27 +0200

    some updates

---
+++ src/GameResult.java
@@ -0,0 +1,5 @@
+public class GameResult {
+    String name;
+    int triesCount;
+    long duration;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,82 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static Random random = new Random();
+    private static List<GameResult> gameResults = new ArrayList<>();
 
     public static void main(String[] args) {
-        int guessRange = 50;
-        int tries = guessRange / 10;
-        int randomizedNumber = random.nextInt(guessRange) + 1;
-        int hintRegion = guessRange / 2;
+        do {
+            System.out.println("Please enter your name");
 
+            String userName = scanner.next();
+
+            System.out.println("Please type the numbers range you would like to make guesses in");
+
+            int guessRange = scanner.nextInt();
+            int tries = (guessRange / 10) + 1;
+            Long startTime = System.currentTimeMillis();
+
+            startGame(userName, guessRange, tries, startTime);
+
+            System.out.println("Enter y/Y if you want to play another game");
+        } while (userAgreed());
+
+        gameResults.forEach(gameResult -> System.out.printf("%s %.2f sec %d tries remaining\n", gameResult.name, gameResult.duration / 1000.0,  gameResult.triesCount));
+    }
+
+    private static boolean userAgreed() {
+        String pressedKey = scanner.next();
+
+        return pressedKey.equals("Y") || pressedKey.equals("y");
+    }
+
+    private static int readUserNum(int guessRange) {
+        int guess = 0;
+        boolean validNumberEntered = false;
+
+        while (!validNumberEntered) {
+            try {
+                guess = scanner.nextInt();
+                if ((guess <= guessRange) && (guess > 0)) {
+                    validNumberEntered = true;
+                } else {
+                    System.out.println("Please input the number in your specified range");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Input a number please");
+                scanner.next();
+            }
+        }
+
+        return guess;
+    }
+
+    private static void startGame(String userName, int guessRange, int tries, Long gameStartTime) {
         boolean hintUsed = false;
         boolean useRegularHint = true;
+        int randomizedNumber = random.nextInt(guessRange) + 1;
+
+        int hintRegion = guessRange / 2;
 
         String hint = randomizedNumber > hintRegion ? "Number is higher than " + hintRegion
                 : "Number is lower than " + hintRegion;
 
         while (tries > 0) {
             System.out.println("Please enter your guess in a numbers range of 1 to " + guessRange);
-            int guess;
-
-            try {
-                guess = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Input a number please");
-                scanner.next();
-                continue;
-            }
+            int guess = readUserNum(guessRange);
 
             if (guess == randomizedNumber) {
                 System.out.println("Your guess was correct, congratulations you won!");
+
+                Long duration = System.currentTimeMillis() - gameStartTime;
+
+                GameResult result = new GameResult();
+                result.name = userName;
+                result.triesCount = tries;
+                result.duration = duration;
+                gameResults.add(result);
+
                 return;
             } else {
                 tries--;
@@ -43,12 +88,10 @@ public class Main {
                 }
 
                 if (!hintUsed) {
-                    System.out.println(tries != 1 ? "Enter Y or y in order to receive a hint"
-                            : "Enter Y or y in order to receive a super hint");
+                    System.out.println(tries != 1 ? "Enter Y/y in order to receive a hint"
+                            : "Enter Y/y in order to receive a super hint");
 
-                    String pressedKey = scanner.next();
-
-                    if (pressedKey.equals("Y") || pressedKey.equals("y")) {
+                    if (userAgreed()) {
                         if (tries == 1 && !useRegularHint) {
                             System.out.println(randomizedNumber + " is your super hint :)");
                         } else {
